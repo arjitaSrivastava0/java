4fff357c091ed1f853b55e88a4257b6215b65eae
arjita.s <arjita.s@zohocorp.com>
2017-02-27 16:36:10 +0530

    multiplication program

--- Basic/src/com/codelab/matrixmultiplication/MatrixMultiplication.java
+++ Basic/src/com/codelab/matrixmultiplication/MatrixMultiplication.java
@@ -1,9 +1,44 @@
 //$Id$
 package com.codelab.matrixmultiplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MatrixMultiplication {
 
-public static void main(String[] args){
-	System.out.println("hello world ");
-}
+/**
+ * @param args
+ * @throws IOException
+ */
+/**
+ * @param args
+ * @throws IOException
+ */
+public static void main(String[] args) throws IOException{
+	
+	int rowA;
+	int colA;
+	int rowB;
+	int colB;
+	int matA;
+	int matB;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter the dimention for matA: ");
+	matA = Integer.parseInt(br.readLine());
+	System.out.println("Enter the row values of first matrix: ");
+	rowA = Integer.parseInt(br.readLine());
+	System.out.println("Enter the column values of first matrix: ");
+	colA = Integer.parseInt(br.readLine());
+	
+	System.out.println("Enter the dimention for matB: ");
+	matB = Integer.parseInt(br.readLine());
+	System.out.println("Enter the row values of second matrix: ");
+	rowB = Integer.parseInt(br.readLine());
+	System.out.println("Enter the column values of second matrix: ");
+	colB = Integer.parseInt(br.readLine());
+	
+	System.out.println("A matrix is: "+rowA+" "+colA);
+	
+	}
 }
