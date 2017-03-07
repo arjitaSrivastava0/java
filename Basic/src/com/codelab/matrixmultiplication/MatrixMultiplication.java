a32384c6cbe8f958883c8d9b14c1f89edc31f618
arjita.s <arjita.s@zohocorp.com>
2017-03-07 23:10:40 +0530

    Completed matrix multiplication

--- Basic/src/com/codelab/matrixmultiplication/MatrixMultiplication.java
+++ Basic/src/com/codelab/matrixmultiplication/MatrixMultiplication.java
@@ -22,25 +22,20 @@ public static void main(String[] args) throws IOException{
 	int colA ;
 	int rowB ;
 	int colB ;
+	int sum = 0;
+	
 	
 	System.out.println("Enter the dimensions of first matrix; ");
 	InputStreamReader inputstream = new InputStreamReader(System.in);
 	BufferedReader br = new BufferedReader(inputstream);
 	String rowAstr = br.readLine();
-	System.out.println("rowA "+rowAstr);
 	rowA = Integer.parseInt(rowAstr);
-	System.out.println("rowA "+rowA);
 	String colAstr = br.readLine();
-	System.out.println("colA "+colAstr);
-	colA = Integer.parseInt(colAstr);
-	System.out.println("colA "+colA);
-	/*System.out.println("Enter a strin: ");
-	StringBuffer name = new StringBuffer(br.readLine()).;
-	System.out.println("reverse name : "+name);*/
+	colA = Integer.parseInt(colAstr);	
 	
+	System.out.println("The dimension of second matrix is: "+rowA+"x"+colA);
 	
 	int matA[][] = new int[rowA][colA];
-	//int matB[][] = matB = new int[i2][j2];
 	
 
 	for(int i = 0; i < rowA; i++){
@@ -65,8 +60,75 @@ public static void main(String[] args) throws IOException{
 	}
 
 
+		System.out.println("Enter the dimenstions of second Matrix: ");
+		String rowBstr = br.readLine();
+		rowB = Integer.parseInt(rowBstr);
+		String colBstr = br.readLine();
+		colB = Integer.parseInt(colBstr);
+		
+		System.out.println("The dimension of first matrix is: "+rowB+"x"+colB);
+		
+		int matB[][] = new int[rowB][colB];
+		
+		for(int i = 0;i < rowB;i++){
+			
+			for(int j = 0;j < colB;j++){
+				
+				String matBstr = br.readLine();
+				matB[i][j] = Integer.parseInt(matBstr);
+			}
+			
+		}
 		
 		
+		for(int i = 0;i < rowB;i++){
+			
+			for(int j = 0;j < colB;j++){
+				
+				System.out.print(" "+matB[i][j]); 
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		int matResult[][] = new int[rowA][colB];
+		
+		if(colA == rowB){
+			
+			for(int i = 0;i<rowA;i++){
+				
+				for(int j = 0;j < colB;j++){
+					
+					for(int k = 0;k < colA;k++){
+						
+						sum += matA[i][k]*matB[k][j];
+						
+					}
+					
+					matResult[i][j] = sum;
+					sum = 0;
+					
+				}
+			}
+
+			for(int i = 0;i < rowA;i++){
+				
+				for(int j = 0;j < colB;j++){
+					
+					System.out.print(+matResult[i][j]+" "); 
+				}
+				
+				System.out.println();
+			}
+
+			
+		}
+		
+		else{
+			System.out.println("Sorry input is not valid for multiplication.");
+		}
 		
 		
 	
